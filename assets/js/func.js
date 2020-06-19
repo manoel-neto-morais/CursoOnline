7d2de0bef79a490252eab4e240dd8bcd0a776449
Manoel Neto <manoel.neto.morais@gmail.com>
2020-06-19 10:27:34 -0300

    Update func.js

--- assets/js/func.js
+++ assets/js/func.js
@@ -1,5 +1,6 @@
 let flag = true
-let cont = 1.0
+let cont = 1
+let flagzoom = true
 
 function contraste() {
     let x = document.getElementById("css")
@@ -14,13 +15,34 @@ function contraste() {
     }
 }
 
+function calibrazoom() {
+
+    if (window.innerWidth < 600) {
+        cont = 0.7
+    }
+
+}
+
 function maiszoom() {
+
+    if (flagzoom) {
+        calibrazoom()
+        flagzoom = false
+    }
+
     let x = document.getElementById("zoom")
     cont += 0.1
     x.style.zoom = cont
 }
 
+
 function menoszoom() {
+
+    if (flagzoom) {
+        calibrazoom()
+        flagzoom = false
+    }
+
     let x = document.getElementById("zoom")
     cont -= 0.1
     x.style.zoom = cont
